a04465e30ff3cc2fc846d6954e726b16da159c94
Tanishq Motke <tmotke98@gmail.com>
2026-07-06 23:51:09 +0100

    AMZQA-1

--- pages/search_result.py
+++ pages/search_result.py
@@ -5,7 +5,6 @@ class ResultPage:
         self.all_search_result = page.locator(".puisg-col-inner")
         
     def verify_the_result(self,product_name):
-        print(self.all_search_result.count())
         self.page.wait_for_load_state("networkidle")
         
         titles = self.all_search_result.locator("h2 span")
